d2ff008d8cc02b1c8410fa9440db75e272597a79
Kevin Agnes <kevinagnes@gmail.com>
2022-01-07 08:58:10 -0300

    Updating

--- UnityProject/Assets/TerrainManager.cs
+++ UnityProject/Assets/TerrainManager.cs
@@ -13,8 +13,10 @@ public class TerrainManager : MonoBehaviour
     [Range(1, 10)]
     public float terrainMaxHeight;
     [Range(1, 32)]
-    public int radius;
+    public int[] radius;
     Terrain terrain;
+    float basePixelError = 1f;
+
     void Start()
     {
         terrain = GetComponent<Terrain>();
@@ -22,6 +24,7 @@ public class TerrainManager : MonoBehaviour
         Vector3 terrainSize = terrain.terrainData.size;
         terrainSize.y = terrainMaxHeight;
         terrain.terrainData.size = terrainSize;
+        terrain.heightmapPixelError = basePixelError;
     }
 
 
@@ -37,36 +40,30 @@ public class TerrainManager : MonoBehaviour
         mappedMouse.x = Mathf.Clamp(mappedMouse.x, 0, xBase-1);
         mappedMouse.y = Mathf.Clamp(mappedMouse.y, 0, yBase-1);
 
-        // this only happens when we press mouse left button
-        if (Input.GetMouseButton(0))
+        // check for all buttons, 
+        // if they are clicked we set pixel error to 5 and start painting
+        // if they are released we set pixel error to 1
+        for (int i = 0; i < 3; i++)
         {
-            Debug.Log(mappedMouse);
-
-            float[,] heights = terrain.terrainData.GetHeights(0, 0, xBase, yBase);
-
-            List<Vector2> points = new List<Vector2>();
-
-            // here we are doing a nested for loop because we need to access all the points
-            // to measure the distance of each one to the origing (which is the current mouse pos)
-            for (int x = 0; x < xBase - 1; x++)
+            // if pressed
+            if (Input.GetMouseButton(i))
             {
-                for (int y = 0;y < yBase -1; y++)
-                {
-                    Vector2 currentPoint = new Vector2(x, y);
-
-                    if (Vector2.Distance(mappedMouse, currentPoint) < radius)
-                    {
-                        points.Add(currentPoint);
-                    }
-                }
+                terrain.heightmapPixelError = basePixelError * 5;
+                Paint(mappedMouse, radius[i]);
+            }
+            // if released
+            if (Input.GetMouseButtonUp(i))
+            {
+                terrain.heightmapPixelError = basePixelError;
             }
 
-            for (int i = 0; i < points.Count; i++)
-            {
-                heights[(int)points[i].x, (int)points[i].y] -= Deepness;
-            }
-            
-            terrain.terrainData.SetHeights(0,0,heights);
+        }
+
+        // this will save the image to app/screenshots and clean the terrain
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SaveImage();
+            CleanDisplay();
         }
 
     }
@@ -107,4 +104,62 @@ public class TerrainManager : MonoBehaviour
         return (NewValue);
     }
 
+    private void Paint(Vector2 mappedMouse, int radius)
+    {
+        float[,] heights = terrain.terrainData.GetHeights(0, 0, xBase, yBase);
+
+        List<Vector2> points = new List<Vector2>();
+
+        // here we are doing a nested for loop because we need to access all the points
+        // to measure the distance of each one to the origing (which is the current mouse pos)
+        for (int x = 0; x < xBase - 1; x++)
+        {
+            for (int y = 0; y < yBase - 1; y++)
+            {
+                Vector2 currentPoint = new Vector2(x, y);
+
+                if (Vector2.Distance(mappedMouse, currentPoint) < radius)
+                {
+                    points.Add(currentPoint);
+                }
+            }
+        }
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            heights[(int)points[i].x, (int)points[i].y] -= Deepness;
+        }
+
+        terrain.terrainData.SetHeights(0, 0, heights);
+    }
+
+
+    private void CleanDisplay()
+    {
+        float[,] z = new float[xBase, yBase];
+
+        for (int x = 0; x < xBase; x++)
+        {
+            for (int y = 0; y < yBase; y++)
+            {
+                z[x, y] = 1; //CalculateHeight(x,y); 
+            }
+        }
+        terrain.terrainData.SetHeights(0, 0, z);
+    }
+
+    private void SaveImage()
+    {
+        Texture2D tex = ScreenCapture.CaptureScreenshotAsTexture();
+        byte[] bytes = tex.EncodeToPNG();
+        Destroy(tex);
+
+        System.IO.File.WriteAllBytes(string.Format("{0}/{1}{2}.png",
+                              Application.dataPath,
+                              "pic_",
+                              System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss")), bytes);
+    }
 }
+
+
+
